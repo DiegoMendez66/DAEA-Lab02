8d9ac1f40f613e7ce57ec51982fcc81025e74c31
Alumno <Alumno@LAB1504-19>
2023-09-05 22:04:29 -0500

    Agregar archivos de proyecto.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string contraseñaCorrecta = "12345";
+            string usuarioCorrecto = "admin";
+
+            string usuario = txtUsername.Text;
+            string contraseña = txtPassword.Password;
+
+            if (contraseña == contraseñaCorrecta && usuario == usuarioCorrecto)
+            {
+                Window1 window1 = new Window1();
+                window1.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Contraseña o usuario incorrecto. Inténtalo de nuevo.");
+            }
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Ingresos_Click(object sender, RoutedEventArgs e)
+        {
+            FormIngresos formIngresos = new FormIngresos();
+            formIngresos.Show();
+            this.Close();
+        }
+
+        private void Salidas_Click(object sender, RoutedEventArgs e)
+        {
+            FormIngresos formIngresos = new FormIngresos();
+            formIngresos.Show();
+            this.Close();
+        }
+
+        private void Conductores_Click(object sender, RoutedEventArgs e)
+        {
+            FormConductores formConductores = new FormConductores();
+            formConductores.Show();
+            this.Close();
+        }
+
+        private void Reporte_Click(object sender, RoutedEventArgs e)
+        {
+            Reportes reportes = new Reportes();
+            reportes.Show();
+            this.Close();
+        }
+    }
+}
